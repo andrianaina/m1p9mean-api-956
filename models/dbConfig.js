4ebfcb484fb8017949671f8237e0e0b510739a4d
Henintsoa Andrianaina <andrianainahenintsoa@gmail.com>
2022-04-11 10:50:23 +0300

    deploy

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,6 +1,14 @@
 const mongoose=require('mongoose');
+// mongoose.connect(
+//     "mongodb://localhost:27017/ekaly",{useNewUrlParser:true,useUnifiedTopology:true},
+//     (err)=>{
+//         if(!err) console.log('Mongodb connected');
+//         else console.log("Connection error: "+ err);
+//     }
+// )
+
 mongoose.connect(
-    "mongodb://localhost:27017/ekaly",{useNewUrlParser:true,useUnifiedTopology:true},
+    "mongodb+srv://Ekaly:mRSa8ry9QbE1TToF@cluster0.z99ra.mongodb.net/ekaly?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true},
     (err)=>{
         if(!err) console.log('Mongodb connected');
         else console.log("Connection error: "+ err);
